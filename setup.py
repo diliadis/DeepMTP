426b668f50251bc93c08db32ed6ffd400da97599
Dimitrios Iliadis <jimmyhliad@gmail.com>
2022-06-25 13:58:02 +0000

    updating version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from io import open
 
 this_directory = path.abspath(path.dirname(__file__))
 
-VERSION = '0.0.7' 
+VERSION = '0.0.8' 
 DESCRIPTION = 'a Deep Learning Framework for Multi-target Prediction'
 
 def readme():
