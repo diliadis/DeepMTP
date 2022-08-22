a74ad6ee864584ac85e0ea088282473d724691e6
Dimitrios Iliadis <jimmyhliad@gmail.com>
2022-08-22 15:42:18 +0000

    testing explicitly adding requirements in the setup file

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from io import open
 
 this_directory = path.abspath(path.dirname(__file__))
 
-VERSION = '0.0.12' 
+VERSION = '0.0.13' 
 DESCRIPTION = 'a Deep Learning Framework for Multi-target Prediction'
 
 def readme():
@@ -27,7 +27,22 @@ setup(
         packages=find_packages(),
         long_description_content_type='text/markdown',
         url='https://github.com/diliadis/DeepMTP',
-        install_requires=requirements,
+        install_requires=[
+            'liac-arff==2.5.0',
+            'matplotlib>=3.5.2',
+            'matplotlib-inline>=0.1.2',
+            'more-itertools>=8.13.0',
+            'numpy>=1.21.6',
+            'pandas>=1.3.5',
+            'pillow>=9.1.1',
+            'prettytable==3.3.0',
+            'scikit-learn>=1.0.2',
+            'scikit-multilearn==0.2.0',
+            'scipy>=1.7.3',
+            'wandb>=0.12.18',
+            'wget==3.2',
+        ]
+        # install_requires=requirements,
         # dependency_links=['https://download.pytorch.org/whl/cpu'],
         # extras_require={
         #     'cpu': ['torch==1.11.0+cpu', 'torchaudio==0.11.0+cpu', 'torchvision==0.12.0+cpu'],
