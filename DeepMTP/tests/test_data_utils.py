183a68e9fd7315f38b860d5f8e35f81acbcbf715
Dimitrios Iliadis <jimmyhliad@gmail.com>
2022-10-22 20:04:27 +0000

    increasing test coverage for data_utils.py

--- DeepMTP/tests/test_data_utils.py
+++ DeepMTP/tests/test_data_utils.py
@@ -19,51 +19,67 @@ import numpy as np
 import pytest
 
 data_format_type_combo_data = [
-    {'format': 'numpy', 'ids_type': 'int'}, 
-    {'format': 'dataframe', 'ids_type': 'int'}, 
-    {'format': 'dataframe', 'ids_type': 'string'}
-    ]
+    {'pass_fail': 'pass', 'format': 'numpy', 'ids_type': 'int'}, 
+    {'pass_fail': 'pass', 'format': 'dataframe', 'ids_type': 'int'}, 
+    {'pass_fail': 'pass', 'format': 'dataframe', 'ids_type': 'string'},
+    {'pass_fail': 'fail', 'data': pd.DataFrame({'instance_id': [0, 0, 1, 1], 'target_id': [0, 1, 0, 1]})}, # less than three columns
+    {'pass_fail': 'fail', 'data': pd.DataFrame({'instance_id': [0, 0, 1, 1], 'ta_id': [0, 1, 0, 1], 'value': [0, 1, 1, 0]})}, # wrong column name
+    {'pass_fail': 'fail', 'data': pd.DataFrame({'instance_id': [0, 0, 1, 1], 'target_id': [0, 1, 0, 1], 'value': ['a', 'b', 'c', 'd']})}, # characters in the value column
+    {'pass_fail': 'fail', 'data': pd.DataFrame({'instance_id': [0.1, 0.2, 0.3, 0.4], 'ta_id': [0, 1, 0, 1], 'value': [0, 1, 1, 0]})}, # wrong column name
+    {'pass_fail': 'fail', 'data': pd.DataFrame({'instance_id': [0, 0, 1, 1], 'ta_id': [0.1, 0.2, 0.3, 0.4], 'value': [0, 1, 1, 0]})}, # wrong column name
+    {'pass_fail': 'fail', 'data': 'lalala'}, # 
+]
 
 @pytest.mark.parametrize('data_format_type_combo_data', data_format_type_combo_data)
 def test_process_interaction_data(data_format_type_combo_data):
-    data_format = data_format_type_combo_data['format']
-    ids_type = data_format_type_combo_data['ids_type']
+    pass_fail = data_format_type_combo_data['pass_fail']
+
     
     num_instances = 1000
     num_targets = 100
     num_instance_features = 2 
-    if ids_type == 'int':
-        data = process_dummy_MLC(num_features=num_instance_features, num_instances=num_instances, num_targets=num_targets, interaction_matrix_format=data_format)
-    else:
-        data = { # three instance features data sources and three interaction data matrices
-        'train': {
-            'y': pd.DataFrame({'instance_id': ['a', 'a', 'b', 'b', 'c', 'c', 'd', 'd'], 'target_id': ['a', 'b', 'a', 'b', 'a', 'b', 'a', 'b'], 'value': [0, 1, 0, 1, 0, 1, 0, 1]}),
-            'X_instance': pd.DataFrame({'id': ['a', 'b', 'c', 'd'], 'features': list(np.random.rand(4, 10))}),
-            'X_target': pd.DataFrame({'id': ['a', 'b'], 'features': list(np.random.rand(2, 10))}),
-        },
-        'val': {
-            'y': pd.DataFrame({'instance_id': ['e', 'e', 'f', 'f'], 'target_id': ['a', 'b', 'a', 'b'], 'value': [0, 1, 0, 1]}),
-            'X_instance': pd.DataFrame({'id': ['e', 'f'], 'features': list(np.random.rand(2, 10))}),
-            'X_target': pd.DataFrame({'id': ['a', 'b'], 'features': list(np.random.rand(2, 10))}),
-        },
-        'test': {
-            'y': pd.DataFrame({'instance_id': ['g', 'g', 'h', 'h', 'i', 'i'], 'target_id': ['a', 'b', 'a', 'b', 'a', 'b'], 'value': [0, 1, 0, 1, 0, 0]}),
-            'X_instance': pd.DataFrame({'id': ['g', 'h', 'i'], 'features': list(np.random.rand(3, 10))}),
-            'X_target': pd.DataFrame({'id': ['a', 'b'], 'features': list(np.random.rand(2, 10))}),
-        }}
-    
-    info = process_interaction_data(data['train']['y'], verbose=False)
-    assert info['original_format'] == 'numpy' if data_format == 'numpy' else 'triplets'
-    assert info['instance_id_type'] == ids_type
-    assert info['target_id_type'] == ids_type
-    
-    if data_format == 'dataframe':
-        assert data['train']['y'].equals(info['data'])
-    else:
-        triplets = [(i, j, data['train']['y'][i, j]) for i in range(data['train']['y'].shape[0]) for j in range(data['train']['y'].shape[1])]
-        temp_df = pd.DataFrame(triplets, columns=['instance_id', 'target_id', 'value'])
-        assert temp_df.equals(info['data'])
 
+        
+    if pass_fail == 'fail':
+        with pytest.raises(Exception):
+            info = process_interaction_data(data_format_type_combo_data['data'], verbose=False)
+    else:
+        try:
+            data_format = data_format_type_combo_data['format']
+            ids_type = data_format_type_combo_data['ids_type']
+            if ids_type == 'int':
+                data = process_dummy_MLC(num_features=num_instance_features, num_instances=num_instances, num_targets=num_targets, interaction_matrix_format=data_format)
+            else:
+                data = { # three instance features data sources and three interaction data matrices
+                'train': {
+                    'y': pd.DataFrame({'instance_id': ['a', 'a', 'b', 'b', 'c', 'c', 'd', 'd'], 'target_id': ['a', 'b', 'a', 'b', 'a', 'b', 'a', 'b'], 'value': [0, 1, 0, 1, 0, 1, 0, 1]}),
+                    'X_instance': pd.DataFrame({'id': ['a', 'b', 'c', 'd'], 'features': list(np.random.rand(4, 10))}),
+                    'X_target': pd.DataFrame({'id': ['a', 'b'], 'features': list(np.random.rand(2, 10))}),
+                },
+                'val': {
+                    'y': pd.DataFrame({'instance_id': ['e', 'e', 'f', 'f'], 'target_id': ['a', 'b', 'a', 'b'], 'value': [0, 1, 0, 1]}),
+                    'X_instance': pd.DataFrame({'id': ['e', 'f'], 'features': list(np.random.rand(2, 10))}),
+                    'X_target': pd.DataFrame({'id': ['a', 'b'], 'features': list(np.random.rand(2, 10))}),
+                },
+                'test': {
+                    'y': pd.DataFrame({'instance_id': ['g', 'g', 'h', 'h', 'i', 'i'], 'target_id': ['a', 'b', 'a', 'b', 'a', 'b'], 'value': [0, 1, 0, 1, 0, 0]}),
+                    'X_instance': pd.DataFrame({'id': ['g', 'h', 'i'], 'features': list(np.random.rand(3, 10))}),
+                    'X_target': pd.DataFrame({'id': ['a', 'b'], 'features': list(np.random.rand(2, 10))}),
+                }}
+            
+            info = process_interaction_data(data['train']['y'], verbose=False)
+            assert info['original_format'] == 'numpy' if data_format == 'numpy' else 'triplets'
+            assert info['instance_id_type'] == ids_type
+            assert info['target_id_type'] == ids_type
+            
+            if data_format == 'dataframe':
+                assert data['train']['y'].equals(info['data'])
+            else:
+                triplets = [(i, j, data['train']['y'][i, j]) for i in range(data['train']['y'].shape[0]) for j in range(data['train']['y'].shape[1])]
+                temp_df = pd.DataFrame(triplets, columns=['instance_id', 'target_id', 'value'])
+                assert temp_df.equals(info['data'])
+        except Exception as exc:
+            assert False
 
 interaction_files_format_check_should_throw_error = [True, False]
 test_check_interaction_files_format_data = [
