060b20706fb22bdea1c0514e891496e6a0478e7c
Dimitrios Iliadis <jimmyhliad@gmail.com>
2022-10-22 15:49:59 +0000

    fixing setting D test

--- DeepMTP/tests/test_dataset.py
+++ DeepMTP/tests/test_dataset.py
@@ -130,15 +130,19 @@ def test_load_process_DP(test_load_process_DP_data):
 		assert data['train']['y'].shape[0] == data['train']['X_instance'].shape[0]
 
 	elif validation_setting == 'D':
-		assert data['train']['y'].shape[0] == data['train']['X_instance'].shape[0]
 		if split_instance_features:
+			assert data['train']['y'].shape[0] == data['train']['X_instance'].shape[0]
 			assert data['val']['y'].shape[0] == data['val']['X_instance'].shape[0]
 			assert data['test']['y'].shape[0] == data['test']['X_instance'].shape[0]
+		else:
+			assert (data['train']['y'].shape[0] + data['val']['y'].shape[0] + data['test']['y'].shape[0]) == data['train']['X_instance'].shape[0]
 		
-		assert data['train']['y'].shape[1] == data['train']['X_target'].shape[0]
 		if split_target_features:
+			assert data['train']['y'].shape[1] == data['train']['X_target'].shape[0]
 			assert data['val']['y'].shape[1] == data['val']['X_target'].shape[0]
 			assert data['test']['y'].shape[1] == data['test']['X_target'].shape[0]
+		else:
+			assert (data['train']['y'].shape[1] + data['val']['y'].shape[1] + data['test']['y'].shape[1]) == data['train']['X_target'].shape[0]
 
 
 def test_load_process_MC():
