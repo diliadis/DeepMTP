108707545cc9dc9902dc841a87503184dab99a11
Dimitrios Iliadis <jimmyhliad@gmail.com>
2022-10-23 12:10:31 +0000

    small fix

--- DeepMTP/tests/test_branch_models.py
+++ DeepMTP/tests/test_branch_models.py
@@ -3,9 +3,9 @@ from torch import nn
 from DeepMTP.branch_models import MLP
 
 test_MLP_data = [
-    {'input_dim': 10, 'output_dim': 15, 'nodes_per_layer': 100, 'num_layers': 2},
-    {'input_dim': 10, 'output_dim': 15, 'nodes_per_layer': 123, 'num_layers': 2},
-    {'input_dim': 10, 'output_dim': 15, 'nodes_per_layer': [100, 200, 300], 'num_layers': 2},
+    {'input_dim': 10, 'output_dim': 15, 'nodes_per_layer': 100, 'num_layers': 2, 'dropout': 0, 'batch_norm': False},
+    {'input_dim': 10, 'output_dim': 15, 'nodes_per_layer': 123, 'num_layers': 2, 'dropout': 0, 'batch_norm': False},
+    {'input_dim': 10, 'output_dim': 15, 'nodes_per_layer': [100, 200, 300], 'num_layers': 2, 'dropout': 0, 'batch_norm': False},
 ]
 
 
@@ -13,7 +13,7 @@ test_MLP_data = [
 def test_MLP(test_MLP_data):
     calculated_nodes_per_layer = []
     
-    model = MLP({}, input_dim=test_MLP_data['input_dim'], output_dim=test_MLP_data['output_dim'], nodes_per_layer=test_MLP_data['nodes_per_layer'], num_layers=test_MLP_data['num_layers'])
+    model = MLP({}, input_dim=test_MLP_data['input_dim'], output_dim=test_MLP_data['output_dim'], nodes_per_layer=test_MLP_data['nodes_per_layer'], num_layers=test_MLP_data['num_layers'], dropout_rate=test_MLP_data['dropout'], batch_norm=test_MLP_data['batch_norm'])
     linear_layers = [layer for layer in model[0] if isinstance(layer, nn.Linear)]    
     
     calculated_nodes_per_layer.append(test_MLP_data['input_dim'])
